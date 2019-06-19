8f706958351886a0838f29e0dcf9e8f8f7904d0c
Nicole Epp <nicole.epp@invenia.ca>
2019-06-19 16:38:40 -0500

    Add Project.toml for docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,17 +2,19 @@ using Documenter, AWSBatch
 
 makedocs(;
     modules=[AWSBatch],
-    format=:html,
+    format=Documenter.HTML(
+        prettyurls=false,
+        assets=[
+            "assets/invenia.css",
+            "assets/logo.png",
+        ],
+    ),
     pages=[
         "Home" => "index.md",
     ],
     repo="https://gitlab.invenia.ca/invenia/AWSBatch.jl/blob/{commit}{path}#L{line}",
     sitename="AWSBatch.jl",
-    authors="Nicole Epp",
-    assets=[
-        "assets/invenia.css",
-        "assets/logo.png",
-    ],
+    authors="Invenia Technical Computing",
     strict = true,
     checkdocs = :none,
 )
