f3e1570152eb37466269ce51b800ec3d764e761d
Curtis Vogt <curtis.vogt@invenia.ca>
2019-12-10 17:00:42 +0000

    Grammar change to log_events docstring

--- src/batch_job.jl
+++ src/batch_job.jl
@@ -207,7 +207,7 @@ end
 """
     log_events(job::BatchJob) -> Union{Vector{LogEvent}, Nothing}
 
-Fetches the logStreamName, fetches the CloudWatch logs and returns a vector of log events.
+Fetches the logStreamName, fetches the CloudWatch logs, and returns a vector of log events.
 If the log stream does not currently exist then `nothing` is returned.
 
 NOTES:
